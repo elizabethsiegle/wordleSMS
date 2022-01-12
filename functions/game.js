7531eaf8f4a276faf32a5f52b6575a9f0ae34574
Lizzie Siegle <esiegle@brynmawr.edu>
2022-01-11 17:26:00 -0800

    ty emily, refactor, obj, memory

--- functions/game.js
+++ functions/game.js
@@ -1,72 +1,78 @@
 // This is your new function. To start, set the name and path on the left.
 const got = require('got');
 let openFile = Runtime.getAssets()['/words.txt'].open; 
-function randomWord(words) {
+const randomWord = (words) => {
     return words[words.length * Math.random() | 0];
 }
-let randWord = randomWord(openFile().toString().split("\n"));
-console.log(`randWord ${randWord}`);
-let wordAsArr = randWord.split('');
-let guessesAttempted = 0;
-let numCorrectLetters = 0;
+
 const maxGuesses = 5;
 const wordLength = 5;
-let dupLetters = [...wordAsArr]; //comma separated letters
-let incorrectLettersArr = [];
-let scoreCard = [];
-for (let i = 1; i < wordLength; i++) {
-    scoreCard.push('⬛');
+
+let gameState = {}; //player
+//set on in-memory obj -> if in firebase/Sync
+const setupPlayer = (playerId) => { //maybe async so openfile() can finish
+  let randWord = randomWord(openFile().toString().split("\n"));
+  //let wordAsArr = randWord.split(''); //wordAsArr, dupLetters calculated from randWord -> could maybe remove
+  gameState[playerId] = {
+    randWord: randWord,
+    // wordAsArr: wordAsArr,
+    guessesAttempted: 0, //init
+    numCorrectLetters: 0, //init
+    dupLetters: [...randWord], //comma separated letters
+    incorrectLettersArr: []
+  }
+  return gameState[playerId]; //reference
 }
-let handleGuess = async (guess) => {
+
+const handleGuess = async (player, guess) => {
+  let newScoreCard = [];
   try {
-    await got(`https://api.dictionaryapi.dev/api/v2/entries/en/${guess}`).then((response) => {
-      if (response.statusCode !== 404) {
-        console.log(`guess ${guess}`);
-        guessesAttempted+=1;
-        console.log(`guessesAttempted ${guessesAttempted}`);
-        let guessAsArray = guess.split('');
-        let newScoreCard = [];
-        for (let i = 0; i < guessAsArray.length; i++) {
-          if (guessAsArray[i] == wordAsArr[i]) {
-            if (dupLetters[i] != null) {
-              numCorrectLetters+=1;
-            }
-            dupLetters[i] = null;
-            newScoreCard.push('🟩');
-          } else if (guessAsArray[i] != wordAsArr[i] && wordAsArr.includes(guessAsArray[i])) {
-            newScoreCard.push('🟨');
-            //numCorrectLetters+=1; //idk man
-          } else {
-            if (!incorrectLettersArr.includes(guess[i])) {
-              incorrectLettersArr.push(guess[i])
-            }
-            newScoreCard.push('⬛')
+    const response = await got(`https://api.dictionaryapi.dev/api/v2/entries/en/${guess}`).json();
+    if (response.statusCode !== 404) {
+      console.log(`guess ${guess} + randword ${player.randWord} in got req `);
+      player.guessesAttempted+=1;
+      console.log(`guessesAttempted: ${player.guessesAttempted}`);
+      //let guessAsArr = guess.split('');
+      for (let i = 0; i < guess.length; i++) {
+        if (guess.charAt(i) == player.randWord.charAt(i)) {
+          if (player.dupLetters[i] != null) {
+            player.numCorrectLetters+=1;
           }
+          player.dupLetters[i] = null;
+          newScoreCard.push('🟩');
+        } else if (guess.charAt(i) != player.randWord.charAt(i) && player.randWord.includes(guess.charAt(i))) {
+          newScoreCard.push('🟨');
+        } else {
+          if (!player.incorrectLettersArr.includes(guess.charAt(i))); {
+            player.incorrectLettersArr.push(guess.charAt(i));
+          }
+          newScoreCard.push('⬛');
         }
-        console.log(`newScoreCard ${newScoreCard}`);
-        scoreCard = newScoreCard;
-        endFunc();
-      } 
-      else { //404 word not in dict
-        newScoreCard = "word not found in dictionary! try again!";
-        scoreCard = newScoreCard;
-        console.log('Word not found!');
       }
-    });
+      console.log(`newScoreCard ${newScoreCard}`);
+      return newScoreCard;
+      
+    } 
+    else { //404 word not in dict
+      newScoreCard = "word not found in dictionary! try again!";
+      console.log('Word not found!');
+      return newScoreCard;
+    }
   }
   catch (err) {
     newScoreCard = "word not found in dictionary! try again!";
-    scoreCard = newScoreCard;
-    console.log('Word not found!')
+    console.log('Word not found!');
+    return newScoreCard;
+    
   }  
 }
 
-const endFunc = () => {
-  if (guessesAttempted >= maxGuesses) { 
+const endFunc = (player, scoreCard) => {
+  if (player.guessesAttempted >= maxGuesses) { 
     console.log(`guessesAttempted >= maxGuesses`);
     return true;
   }
-  else if (numCorrectLetters == wordLength) {
+  else if (player.numCorrectLetters == wordLength) {
     console.log("in numCorrect");
     return true;
   }
@@ -75,42 +81,39 @@ const endFunc = () => {
     return true;
   }
   else {
+    console.log(`game still going `);
     return false;
   }
 }
   
 exports.handler = async function(context, event, callback) {
-  // Here's an example of setting up some TWiML to respond to with this function
 	let twiml = new Twilio.twiml.MessagingResponse();
   let guess = event.Body.toLowerCase().trim();
-    if (guess.length == wordLength) { //5 letters
-      await handleGuess(guess); //guessesAttempted increments
-      if(endFunc()) { //over, win
-        if(guess == randWord) {
-        twiml.message(`Nice🔥! You guessed the right word in ${guessesAttempted}/${maxGuesses} guesses. You can play again by sending a 5-letter word to guess a new random word👀`);
-        randWord = randomWord(openFile().toString().split("\n"));
-        console.log(`randWord ${randWord}`);
-        wordAsArr = randWord.split('');
-        guessesAttempted = 0;
-        numCorrectLetters = 0;
-        scoreCard = `⬛,⬛,⬛,⬛,⬛`;
-        }
-        else if (guess != randWord) { //over, lose
-          twiml.message(`Game over🙈\nThe correct word was ${randWord}. Send a 5-letter guess to play again!`); 
-          randWord = randomWord(openFile().toString().split("\n"));
-          console.log(`randWord ${randWord}`);
-          wordAsArr = randWord.split('');
-          guessesAttempted = 0;
-          numCorrectLetters = 0;
-          scoreCard = `⬛,⬛,⬛,⬛,⬛`;
-        }
+  let player = gameState[event.From];
+  if (!player) { //new player
+    player = setupPlayer(event.From);
+  }
+  if (guess.length == wordLength) { //5 letters
+    let scoreCard = await handleGuess(player, guess); //guessesAttempted increments
+    console.log(`scoreCard ${scoreCard}`);
+    if(endFunc(player, scoreCard)) { //over, win
+      if(guess == player.randWord) {
+        twiml.message(`Nice🔥! You guessed the right word in ${player.guessesAttempted}/${maxGuesses} guesses. You can play again by sending a 5-letter word to guess a new random word👀`);
+        delete gameState[event.From];
       }
-      else { //keep guessing, not over
-        twiml.message(`${scoreCard.toString()} \n${guessesAttempted}/${maxGuesses} guesses`);
+      else if (guess != player.randWord) { //over, lose
+        twiml.message(`Game over🙈\nThe correct word was ${player.randWord}. Send a 5-letter guess to play again!`); 
+        delete gameState[event.From];
       }
     }
-    else { //not 5 letters
-      twiml.message(`"${guess}" is not valid. Please send a word in the dictionary that is 5 letters please to get started!`);
+    else { //keep guessing, not over
+      twiml.message(`${scoreCard.toString()} \n${player.guessesAttempted}/${maxGuesses} guesses`);
+      console.log(`randWord in not over ${player.randWord}`);
     }
+  }
+  else { //not 5 letters
+    twiml.message(`"${guess}" is not valid. Please send a word in the dictionary that is 5 letters please to get started!`);
+    console.log(`randWord ${player.randWord} in invalid `)
+  }
   return callback(null, twiml);
 };
